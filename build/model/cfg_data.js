935343485f74081b98faa9e6cf4b83a335cc8a34
Quyet_Thanh <115696569+thanhquyet24ptit@users.noreply.github.com>
2023-12-02 17:12:48 +0700

    first commit

---
+++ build/model/cfg_data.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const usedSchema = new mongoose.Schema({
+    recordTime: { 
+        type: Date,
+        default: Date.now
+    },
+    electric: { 
+        type: String,
+        required: true
+    },
+    water: {
+        type: String,
+        required: true
+    },
+    ePrice: {
+        type: String,
+        required: true
+    },
+    wPrice: {
+        type: String,
+        required: true
+    }
+});
+
+const mainSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    used: [usedSchema]
+});
+
+module.exports = mongoose.model("Home", mainSchema);
